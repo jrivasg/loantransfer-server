6f584b6bf3f12ea038002dbf5b57225443f50a38
jrivasg <jrivasgonzalez@gmail.com>
2021-03-22 14:54:47 +0100

    Docker funcionando

--- helpers/init_redis.js
+++ helpers/init_redis.js
@@ -1,9 +1,6 @@
 const redis = require("redis");
 
-const client = redis.createClient({
-  port: 6379,
-  host: "127.0.0.1",
-});
+const client = redis.createClient(process.env.REDIS_URL);
 
 client.on("connect", () => {
   console.log("Client connected to redis...");
