56f9569d75633f8e3535e998a9a9cf74b6148339
Rivas <jrivasgonzalez@gmail.com>
2021-05-30 20:52:32 +0200

    Update User.Controller.js

--- Controllers/User.Controller.js
+++ Controllers/User.Controller.js
@@ -4,7 +4,7 @@ const User = require("../Models/User.model");
 module.exports = {
   getAccountInfo: async (req, res, next) => {
     try {
-      console.log(req.payload);
+      //console.log(req.payload);
       const user = await User.findById(req.payload.aud);
       res.status(200).json(user);
     } catch (error) {
