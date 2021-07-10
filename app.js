239382ffc91aab8b01a748bece3c34b149d81f1a
Rivas <jrivasgonzalez@gmail.com>
2021-07-10 09:35:38 +0200

    cors

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: [
-      "http://localhost",
-      "http://localhost:3000",
-      "http://localhost:9000",
-      "https://85e90cd16ada.ngrok.io",
-    ],
+    origin: *,
   },
 });
 require("./helpers/socket_io")(io);
