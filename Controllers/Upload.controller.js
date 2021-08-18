b0114eb4a22b7498ae130587d9240fb584f5b24f
Rivas <jrivasgonzalez@gmail.com>
2021-08-18 11:32:19 +0200

    Carga de archivos añadida

--- Controllers/Upload.controller.js
+++ Controllers/Upload.controller.js
@@ -1,9 +1,56 @@
+const Project = require("../models/Project.model");
+const fileSystem = require('fs');
+const path = require('path');
+
 module.exports = {
-  upload: async (req, res, next) => {
-    try {
-      
-    } catch (error) {
-      next(error);
-    }
-  },
+    savefiles: async (req, res) => {
+        const { user_id, project_id } = req.body;
+        req.files.forEach(file => {
+            file.uploadBy = user_id;
+            Project.findByIdAndUpdate(project_id, { $push: { docs: file } },
+                (err, doc) => {
+                    if (err) res.status(500).json(err);
+                })
+        });
+        res.status(200).json('Archivo/s guardado/s');
+    },
+    getAllBidFiles: async (req, res) => {
+        const { project_id } = req.body;
+        const project = await Project.findById(project_id).lean().catch(err => { return res.status(500).json(err) });
+
+        project && res.status(200).json(project.docs);
+    },
+    getFile: async (req, res) => {
+        const { doc_id, project_id } = req.query;
+        const project = await Project.findById(project_id).lean();
+        const doc = project.docs.find(doc => String(doc._id) === String(doc_id));
+
+        var filePath = path.join(doc.path);
+        var stat = fileSystem.statSync(filePath);
+
+        res.writeHead(200, {
+            'Content-Type': doc.mimetype,
+            'Content-Length': doc.size
+        });
+
+        var readStream = fileSystem.createReadStream(filePath);
+        // We replaced all the event handlers with a simple call to readStream.pipe()
+        readStream.pipe(res);
+    },
+    deleteFile: async (req, res) => {
+        const { project_id, doc_id, doc_path } = req.query;
+
+        fileSystem.unlink(doc_path, async (err) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json(err);
+            }
+            Project.findByIdAndUpdate(project_id, { $pull: { docs: { _id: doc_id } } },
+                { new: true },
+                (err, project) => {
+                    if (err) return res.status(500).json(err);
+                    res.status(200).json(project.docs);
+                })
+        });
+    },
 };
--- Routes/Upload.route.js
+++ Routes/Upload.route.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const UploadController = require("../Controllers/Upload.controller");
+const uploadController = require("../Controllers/Upload.controller");
 
-router.post("/", UploadController.upload);
+const fs = require("fs");
+const multer = require('multer');
+const DIR = './uploads/';
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log(DIR + req.body.project_id);
+        !fs.existsSync(DIR + req.body.project_id) && fs.mkdirSync(DIR + req.body.project_id);
+        cb(null, DIR + req.body.project_id)
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+var upload = multer({ storage: storage })
+
+router.post("/savedocuments", upload.array('docs'), uploadController.savefiles);
+router.get("/document",  /* autJWT.verifyToken, */ uploadController.getFile);
+router.post(
+  "/documents",
+  /* autJWT.verifyToken, */ uploadController.getAllBidFiles
+);
+router.delete("/deldocument", /* autJWT.verifyToken, */ uploadController.deleteFile);
 
 module.exports = router;
