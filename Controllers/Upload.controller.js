c63fb3eabd70604749805502f14abe9a53da6aa0
Rivas <jrivasgonzalez@gmail.com>
2021-09-07 08:13:37 +0200

    subida archivos modificada

--- Controllers/Upload.controller.js
+++ Controllers/Upload.controller.js
@@ -1,56 +1,61 @@
-//const Project = require("../models/Project.model");
-const fileSystem = require('fs');
-const path = require('path');
+const Bid = require("../Models/bid.model");
+const fileSystem = require("fs");
+const path = require("path");
 
 module.exports = {
-    savefiles: async (req, res) => {
-        const { user_id, project_id } = req.body;
-        req.files.forEach(file => {
-            file.uploadBy = user_id;
-            /* Project.findByIdAndUpdate(project_id, { $push: { docs: file } },
-                (err, doc) => {
-                    if (err) res.status(500).json(err);
-                }) */
-        });
-        res.status(200).json('Archivo/s guardado/s');
-    },
-    getAllBidFiles: async (req, res) => {
-        //const { project_id } = req.body;
-        //const project = await Project.findById(project_id).lean().catch(err => { return res.status(500).json(err) });
-console.log(req.body)
-         res.status(200).json([]);
-    },
-/*     getFile: async (req, res) => {
-        const { doc_id, project_id } = req.query;
-        const project = await Project.findById(project_id).lean();
-        const doc = project.docs.find(doc => String(doc._id) === String(doc_id));
+  savefiles: async (req, res) => {
+    const { bid_id } = req.body;
+    req.files.forEach((file) => {
+      file.uploadBy = req.payload.aud;
+      Bid.findByIdAndUpdate(bid_id, { $push: { documentation: file } }, (err, doc) => {
+        if (err) res.status(500).json(err);
+      });
+    });
+    res.status(200).json("Archivo/s guardado/s");
+  },
+  getAllBidFiles: async (req, res) => {
+    const { bid_id } = req.body;
+    const bid = await Bid.findById(bid_id)
+      .lean()
+      .catch((err) => {
+        return res.status(500).json(err);
+      });
+    //console.log(req.body);
+    bid && res.status(200).json(bid.documentation);
+  },
+  getFile: async (req, res) => {
+    const { doc_id, bid_id } = req.query;
+    const bid = await Project.findById(bid_id).lean();
+    const doc = bid.documentation.find((doc) => String(doc._id) === String(doc_id));
+    var filePath = path.join(doc.path);
+    var stat = fileSystem.statSync(filePath);
 
-        var filePath = path.join(doc.path);
-        var stat = fileSystem.statSync(filePath);
+    res.writeHead(200, {
+      "Content-Type": doc.mimetype,
+      "Content-Length": stat.size,
+    });
 
-        res.writeHead(200, {
-            'Content-Type': doc.mimetype,
-            'Content-Length': doc.size
-        });
-
-        var readStream = fileSystem.createReadStream(filePath);
-        // We replaced all the event handlers with a simple call to readStream.pipe()
-        readStream.pipe(res);
-    },
-    deleteFile: async (req, res) => {
-        const { project_id, doc_id, doc_path } = req.query;
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(res);
+  },
+  deleteFile: async (req, res) => {
+        const { bid_id, doc_id, doc_path } = req.query;
 
         fileSystem.unlink(doc_path, async (err) => {
             if (err) {
                 console.error(err)
                 return res.status(500).json(err);
             }
-            Project.findByIdAndUpdate(project_id, { $pull: { docs: { _id: doc_id } } },
-                { new: true },
-                (err, project) => {
-                    if (err) return res.status(500).json(err);
-                    res.status(200).json(project.docs);
-                })
+            Bid.findByIdAndUpdate(
+              bid_id,
+              { $pull: { documentation: { _id: doc_id } } },
+              { new: true },
+              (err, bid) => {
+                if (err) return res.status(500).json(err);
+                res.status(200).json(bid.documentation);
+              }
+            );
         });
-    }, */
+    },
 };
--- Routes/Upload.route.js
+++ Routes/Upload.route.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const uploadController = require("../Controllers/Upload.controller");
+const autJWT = require("../helpers/jwt_helper");
 
 const fs = require("fs");
-const multer = require('multer');
-const DIR = './uploads/';
+const multer = require("multer");
+const DIR = "./uploads/";
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        console.log(DIR + req.body.project_id);
-        !fs.existsSync(DIR + req.body.project_id) && fs.mkdirSync(DIR + req.body.project_id);
-        cb(null, DIR + req.body.project_id)
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
+  destination: function (req, file, cb) {
+    console.log(DIR + req.body.bid_id);
+    !fs.existsSync(DIR + req.body.bid_id) &&
+      fs.mkdirSync(DIR + req.body.bid_id);
+    cb(null, DIR + req.body.bid_id);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-var upload = multer({ storage: storage })
+var upload = multer({ storage: storage });
 
-router.post("/savedocuments", upload.array('docs'), uploadController.savefiles);
-//router.get("/document",  /* autJWT.verifyToken, */ uploadController.getFile);
+router.post("/savedocuments", upload.array("docs"), uploadController.savefiles);
+router.get("/document", autJWT.verifyAccessToken, uploadController.getFile);
 router.post(
   "/getAll",
-  /* autJWT.verifyToken, */ uploadController.getAllBidFiles
+  autJWT.verifyAccessToken,
+  uploadController.getAllBidFiles
 );
 //router.delete("/deldocument", /* autJWT.verifyToken, */ uploadController.deleteFile);
 
